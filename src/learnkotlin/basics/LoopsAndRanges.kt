da45df1bd938540f42a6780cffd5effe0f072496
Muhammad Hewedy <mhewedy@gmail.com>
2019-05-10 13:57:51 +0300

    add Loops and Arrays file

---
+++ src/learnkotlin/basics/LoopsAndRanges.kt
@@ -0,0 +1,12 @@
+package learnkotlin.basics
+
+fun main(args: Array<String>) {
+
+    for (index in 1..100) {
+        println(index)
+    }
+
+    for (l in 10.rangeTo(40L)) {
+        println(l)
+    }
+}
--- src/learnkotlin/basics/Variables.kt
+++ src/learnkotlin/basics/Variables.kt
@@ -1,2 +1,13 @@
 package learnkotlin.basics
 
+fun main(args: Array<String>) {
+
+    val streetNumber = 10
+    val streetName = "High Street"
+
+    println(streetNumber)
+    println(streetName)
+
+
+
+}
