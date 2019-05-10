079e635496b13ee537d0047b6d118dd0a289578e
Muhammad Hewedy <mhewedy@gmail.com>
2019-05-10 14:47:22 +0300

    conditionals

--- src/learnkotlin/basics/Conditionals.kt
+++ src/learnkotlin/basics/Conditionals.kt
@@ -5,8 +5,9 @@ fun main(args: Array<String>) {
     // if is an expression
 
     val grade = "90".toInt()
-    val feedback = if (grade >= 90) {
-        "Excellent"
+    val feedback: Any = if (grade >= 90) {
+        println("Excellent")
+        30
     } else "Not so excellent"
 
     println(feedback)
