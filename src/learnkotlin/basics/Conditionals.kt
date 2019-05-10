594c63c679c0069a1d02daee9ce2d9934e223439
Muhammad Hewedy <mhewedy@gmail.com>
2019-05-10 14:57:11 +0300

    conditionals

--- src/learnkotlin/basics/Conditionals.kt
+++ src/learnkotlin/basics/Conditionals.kt
@@ -7,8 +7,28 @@ fun main(args: Array<String>) {
     val grade = "90".toInt()
     val feedback: Any = if (grade >= 90) {
         println("Excellent")
-        30
+        100
     } else "Not so excellent"
 
     println(feedback)
+
+    /// ----- when
+    println("practicing when ... ****")
+
+    // when also is an expression
+    val result = when (feedback) {
+        100 -> {
+            println("Good $feedback")
+            1
+        }
+        "Excellent" -> {
+            println(feedback.toString().toUpperCase())
+            2
+        }
+        is String -> println("it is a string")
+        is Int -> println("it is an Int")
+        else -> "Nothing"
+    }
+
+    println(result)
 }
