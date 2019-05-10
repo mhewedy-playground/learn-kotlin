1a29bf08a6af0b3091c0935db55326328a21ba02
Muhammad Hewedy <mhewedy@gmail.com>
2019-05-10 21:50:29 +0300

    classes

--- src/learnkotlin/classes/EnumClasses.kt
+++ src/learnkotlin/classes/EnumClasses.kt
@@ -1,10 +1,24 @@
 package learnkotlin.classes
 
 enum class Priority(/*it can be `val` as well*/var value: Int) {
-    MINOR(-1),
-    NORMAL(0),
-    MAJOR(1),
-    CRITICAL(10)
+    MINOR(-1) {
+        override fun size() = 100
+    },
+    NORMAL(0) {
+        override fun size() = 200
+    },
+    MAJOR(1) {
+        override fun size() = 300
+    },
+    CRITICAL(10) {
+        override fun size() = 400
+    };
+
+    override fun toString(): String {
+        return "(name=$name, value=$value, ordinal=$ordinal), size: ${size()}"
+    }
+
+    abstract fun size(): Int
 }
 
 fun main(args: Array<String>) {
@@ -13,5 +27,11 @@ fun main(args: Array<String>) {
 
     priority.value = 30
 
-    Priority.values().forEach { println("$it, value=${it.value}, ordinal: ${it.ordinal}") }
+    // values() returns an Array, which we can use forEach on
+    /*Priority.values().forEach { println(it) }*/
+
+    // the values() returns an array which is an iterable (has the method iterator())
+    for (priority in Priority.values()) {
+        println(priority)
+    }
 }
