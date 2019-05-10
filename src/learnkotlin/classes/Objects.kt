5c640e9d6d6acc62bbe10e960a4e1e8a554811b1
Muhammad Hewedy <mhewedy@gmail.com>
2019-05-10 22:07:39 +0300

    classes

--- src/learnkotlin/classes/Objects.kt
+++ src/learnkotlin/classes/Objects.kt
@@ -1,2 +1,22 @@
 package learnkotlin.classes
 
+object Global {
+    const val PI = 3.14159
+}
+
+class Abc {
+    companion object {
+        const val PI = 3.14
+    }
+}
+
+fun main(args: Array<String>) {
+
+    val localObject = object {
+        /*const << const not allowed here*/  val PI = 3.14
+    }
+
+    println("global ${Global.PI}")
+    println("local ${localObject.PI}")
+    println("Abc ${Abc.PI}")
+}
