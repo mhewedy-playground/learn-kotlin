29751c2f8a107288dfe4ba3cc19570b4ff9e4108
Muhammad Hewedy <mhewedy@gmail.com>
2019-05-10 17:59:39 +0300

    classes

--- src/learnkotlin/classes/VisibilityModifiers.kt
+++ src/learnkotlin/classes/VisibilityModifiers.kt
@@ -2,6 +2,11 @@ package learnkotlin.classes
 
 // default visibility modifier is public
 
+// public (every where)
+// internal (module, maven or gradle module)
+// private  (inside the file for global objects, and inside the class for class members)
+// protected (for class members only; same as private but accessible from sub classes)
+
 fun main(args: Array<String>) {
 
 }
