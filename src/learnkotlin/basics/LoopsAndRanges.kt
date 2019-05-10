b94bf4e657f45a6d5c74f8fd4008ba85d5565c0f
Muhammad Hewedy <mhewedy@gmail.com>
2019-05-10 14:16:56 +0300

    add Loops and Arrays file

--- src/learnkotlin/basics/LoopsAndRanges.kt
+++ src/learnkotlin/basics/LoopsAndRanges.kt
@@ -3,10 +3,46 @@ package learnkotlin.basics
 fun main(args: Array<String>) {
 
     for (index in 1..100) {
-        println(index)
+        print("$index, ")
     }
+    println()
 
     for (l in 10.rangeTo(40L)) {
-        println(l)
+        print("$l, ")
     }
+    println()
+
+    val aRange: LongRange = 10..30L
+    println(aRange.first)
+    println(aRange.step)
+
+    aRange.forEach { print("$it, ") }
+    println()
+
+    // for is not an expression
+    /*  // compilation error
+    val x = for (i in 1..10) {
+        i * i
+    }
+    */
+
+    println("range is an iterable and can be converted to list and we can get an iterator from the range")
+
+    val range: IntRange = 1.rangeTo(100)
+    val iterable: Iterable<Int> = 1.rangeTo(100)
+
+    val list = range.toList()       // it iterates over the range to create a list (expensive)
+    val iterator: IntIterator = range.iterator()
+
+    println(range)
+    println(iterable)
+    println(list)
+    println(iterator)
+
+    // -------------------
+
+    for (i in 100 downTo 10 step 10) {
+        println(i)
+    }
+
 }
