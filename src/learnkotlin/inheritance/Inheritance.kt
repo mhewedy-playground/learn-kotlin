bc8c072d772c8cae37919718f77f921d5fb5655a
Muhammad Hewedy <mhewedy@gmail.com>
2019-05-11 03:49:15 +0300

    inheritance

--- src/learnkotlin/inheritance/Inheritance.kt
+++ src/learnkotlin/inheritance/Inheritance.kt
@@ -1,15 +1,27 @@
 package learnkotlin.inheritance
 
-open class Person { // extendable classes should be declared `open`
+open class Person(var firstName: String, var lastName: String) { // extendable classes should be declared `open`
 
-    open fun validate() {       // overridable function should be declared `open`
 
+    open fun validate(): Boolean {       // overridable function should be declared  `open`
+        return firstName.isNotBlank() && lastName.isNotBlank()
     }
 }
 
-class Customer : Person() {
+enum class App {
+    ERP, CRM
+}
 
-    override fun validate() {
+class CrmCustomer(firstName: String, lastName: String, val clientApp: App = App.CRM) :
+    Person(firstName, lastName) {
 
-    }
+    override fun validate() = super.validate() && clientApp == App.CRM
+}
+
+fun main(args: Array<String>) {
+    val c1 = CrmCustomer("abdullah", "hewedy")
+    println(c1.validate())
+
+    val c2 = CrmCustomer("abdullah", "hewedy", App.ERP)
+    println(c2.validate())
 }
