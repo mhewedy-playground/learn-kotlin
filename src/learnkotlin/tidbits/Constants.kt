f0e21999e4ba49ff8bab30deb5fce1abca459d0d
Muhammad Hewedy <mhewedy@gmail.com>
2019-05-14 09:53:52 +0300

    tidbit

---
+++ src/learnkotlin/tidbits/Constants.kt
@@ -0,0 +1,15 @@
+package learnkotlin.tidbits
+
+const val CameraRecordSettingsPixelCount = 30
+/*const */val myConst = callSomeFunction()      // cannot define const on non compile-time constant, like function call
+
+fun callSomeFunction() = 10
+
+object CameraRecordSettings {
+    const val pixelCount = 30
+}
+
+fun main(args: Array<String>) {
+    CameraRecordSettings.pixelCount
+    CameraRecordSettingsPixelCount
+}
