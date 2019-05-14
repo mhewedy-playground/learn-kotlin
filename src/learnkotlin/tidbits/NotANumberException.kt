b3c22499e08cfa961ee1372dcbc3894a9d0c19d3
Muhammad Hewedy <mhewedy@gmail.com>
2019-05-14 09:45:14 +0300

    tidbit

--- src/learnkotlin/tidbits/NotANumberException.kt
+++ src/learnkotlin/tidbits/NotANumberException.kt
@@ -10,9 +10,15 @@ fun checkIsNumber(num: Any) {
 
 fun main(args: Array<String>) {
 
-    try {
+    val result = try {
         checkIsNumber("40")
+        40 + 40
     } catch (e: NotANumberException) {
         println("${e.message}")
+    }finally {
+        -1
     }
+
+    println(result) // will print Kotlin.Unit cause the expression above throws an exception
+    // ... and the finally block's value is ignored ... only the try block or the catch block values are considered.
 }
