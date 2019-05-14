be8dfffd27b5150f01f9209ed487fbb5acb9f5fd
Muhammad Hewedy <mhewedy@gmail.com>
2019-05-14 03:57:31 +0300

    tidbit

--- src/learnkotlin/tidbits/NotANumberException.kt
+++ src/learnkotlin/tidbits/NotANumberException.kt
@@ -1,14 +1,18 @@
 package learnkotlin.tidbits
 
-class NotANumberException : Throwable()
+class NotANumberException(message: String) : Throwable(message)
 
 fun checkIsNumber(num: Any) {
     when (num) {
-        !is Number -> throw NotANumberException()
+        !is Number -> throw NotANumberException("Is not a number")
     }
 }
 
 fun main(args: Array<String>) {
 
-    checkIsNumber(50L)
+    try {
+        checkIsNumber("40")
+    } catch (e: NotANumberException) {
+        println("${e.message}")
+    }
 }
