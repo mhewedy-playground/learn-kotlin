fa7a5464c3f744433ec7e22a74c62e4b235753cc
Muhammad Hewedy <mhewedy@gmail.com>
2019-05-16 02:22:17 +0300

    stdlib

---
+++ src/learnkotlin/stdlib/StringExtensions.kt
@@ -0,0 +1,18 @@
+package learnkotlin.stdlib
+
+fun main() {
+
+    with("Hello") {
+        println(length)
+        println(toUpperCase())
+    }
+
+
+    val run: Int = "hello".run { this.length }
+
+    val apply: String = "hello".apply { this.length }
+
+    val let: Int = "hello".let { it.length }
+
+    val also: String = "hello".also { it.length }
+}
