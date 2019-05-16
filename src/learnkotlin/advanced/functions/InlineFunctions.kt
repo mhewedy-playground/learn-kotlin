e95acb4727fdec6a11dd0650564f9f6a1b4dd69a
Muhammad Hewedy <mhewedy@gmail.com>
2019-05-16 11:20:01 +0300

    function

--- src/learnkotlin/advanced/functions/InlineFunctions.kt
+++ src/learnkotlin/advanced/functions/InlineFunctions.kt
@@ -1,5 +1,9 @@
 package learnkotlin.advanced.functions
 
+inline fun nOp(x: Int) {        // redundant
+    println("x is $x")
+}
+
 inline fun operation(op: () -> Unit): Unit {
     println("before call op")
     op()
@@ -10,4 +14,6 @@ fun main() {
     operation {
         println("in the op function")
     }
+
+    nOp(10)
 }
