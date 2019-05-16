f25d5fca52d0ecfa6bf4fb0d6904e2ffa23555f6
Muhammad Hewedy <mhewedy@gmail.com>
2019-05-16 22:27:33 +0300

    function

--- src/learnkotlin/advanced/functions/LocalReturns.kt
+++ src/learnkotlin/advanced/functions/LocalReturns.kt
@@ -3,7 +3,7 @@ package learnkotlin.advanced.functions
 fun innerFunctionCanReturnItself() {
 
     (fun() {
-        println("executing Self-invoking function")
+        //println("executing Self-invoking function")
         return  // same as return@f (i.e. return from the local function)
     }())
 
@@ -18,8 +18,17 @@ fun lambdaCannotReturnItself_OnlyTheSurroundingFunction() {
     println("hello yes inside: lambdaCannotReturnItself_OnlyTheSurroundingFunction")
 }
 
+fun returnFromLambda() {
+    1.rangeTo(10).forEach {
+        it * it
+        return@forEach
+    }
+    println("hello yes inside: returnFromLambda")
+}
+
 fun main() {
 
     innerFunctionCanReturnItself()
     lambdaCannotReturnItself_OnlyTheSurroundingFunction()
+    returnFromLambda()
 }
