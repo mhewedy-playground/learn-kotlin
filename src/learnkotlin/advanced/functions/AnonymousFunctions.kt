286aa3c02056458effd9676419bad74a3399be44
Muhammad Hewedy <mhewedy@gmail.com>
2019-05-17 12:37:13 +0300

    function

--- src/learnkotlin/advanced/functions/AnonymousFunctions.kt
+++ src/learnkotlin/advanced/functions/AnonymousFunctions.kt
@@ -1,14 +1,17 @@
 package learnkotlin.advanced.functions
 
-fun op(x: Int, op: (Int) -> Int) = op(x)
+fun op(x: Int, op: (Int) -> Int) = op(x)        // carrying
 
-fun fx(x1: Int): (Int) -> (Int) -> Int {
+fun fx(x1: Int): (Int) -> (Int) -> Int {        // carrying
     return fun(x2): (Int) -> Int {
         return fun(x3): Int {
             return x1 * x2 * x3
         }
     }
 }
+
+val myFun1 = fun(x: Any?) = println(x)
+fun myFun2(x: Any?) = println(x)
 
 fun main() {
     val output = op(10, fun(x): Int {
@@ -27,5 +30,10 @@ fun main() {
     println(result)
 
     println(fx(10)(10)(10))
+
+    /////
+
+    myFun1(10)
+    myFun2(10)
 
 }
