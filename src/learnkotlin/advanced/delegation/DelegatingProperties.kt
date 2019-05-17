1e9a803636a6a13b7a0d94297a64e581415b8024
Muhammad Hewedy <mhewedy@gmail.com>
2019-05-18 02:55:24 +0300

    classes

--- src/learnkotlin/advanced/delegation/DelegatingProperties.kt
+++ src/learnkotlin/advanced/delegation/DelegatingProperties.kt
@@ -1,2 +1,30 @@
 package learnkotlin.advanced.delegation
 
+import kotlin.reflect.KProperty
+
+var myProperty: String by PropertyDelegator
+
+object PropertyDelegator {
+
+    var value = "<empty>"
+
+    operator fun getValue(source: Nothing?, property: KProperty<*>): String {
+        log(source, property)
+        return value
+    }
+
+    operator fun setValue(source: Nothing?, property: KProperty<*>, s: String) {
+        log(source, property)
+        value = s
+    }
+
+    private fun log(source: Nothing?, property: KProperty<*>) {
+        println("nothing: \"$source\", property: \"$property\"")
+    }
+}
+
+fun main() {
+    println("value is $myProperty")
+    myProperty = "10"
+    println("value is $myProperty")
+}
