2ca9839c0be565fa1532e71e6e846121f4d26f61
Muhammad Hewedy <mhewedy@gmail.com>
2019-05-17 17:44:07 +0300

    classes

--- src/learnkotlin/advanced/classes/AlgebricDataTypes.kt
+++ src/learnkotlin/advanced/classes/AlgebricDataTypes.kt
@@ -1,8 +1,14 @@
 package learnkotlin.advanced.classes
 
+import learnkotlin.advanced.classes.Position.*
+
 sealed class Tree(val value: Int)
 class Leaf(value: Int) : Tree(value)
 class Node(value: Int, val left: Tree, val right: Tree) : Tree(value)
+
+enum class Position {
+    CENTER, LEFT, RIGHT
+}
 
 fun getNode(): Tree {
     // ....
@@ -19,18 +25,18 @@ fun main() {
     print(getNode())
 }
 
-fun print(node: Tree, level: Int = 0, pos: String = "center") {
+fun print(node: Tree, level: Int = 0, pos: Position = CENTER) {
     when (node) {
         is Leaf -> printNode(node, level, pos)
         is Node -> {
-            print(node.left, level + 1, "left")
+            print(node.left, level + 1, LEFT)
             printNode(node, level, pos)
-            print(node.right, level + 1, "right")
+            print(node.right, level + 1, RIGHT)
         }
     }
 }
 
-private fun printNode(node: Tree, level: Int, pos: String) {
+private fun printNode(node: Tree, level: Int, pos: Position) {
     println("leaf= ${node.value}, at level: $level and post: $pos")
 }
 
