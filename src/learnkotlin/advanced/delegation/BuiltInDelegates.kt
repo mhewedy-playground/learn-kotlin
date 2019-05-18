12e9aa87f8933f3e4077ce8253b99c4f0d361411
Muhammad Hewedy <mhewedy@gmail.com>
2019-05-18 12:51:42 +0300

    delegation

--- src/learnkotlin/advanced/delegation/BuiltInDelegates.kt
+++ src/learnkotlin/advanced/delegation/BuiltInDelegates.kt
@@ -8,7 +8,18 @@ var myVariable by observable(10) { p, o, n ->
     println("${p.name} was $o and now become $n")
 }
 
-var sitterAge by vetoable(0) { _, _, n -> n in 20..60 }
+/*
+// use this to write some logic in the setter
+var sitterAge by vetoable(0, fun(_, _, n): Boolean {
+    val ageRange = 20..60
+    if (n !in ageRange) {
+        throw Exception("sitter age should be in $ageRange")
+    }
+    return true
+})
+*/
+
+var sitterAge by vetoable(0, { _, _, n -> n in 20..60 })
 
 fun main() {
     myVariable = 30
