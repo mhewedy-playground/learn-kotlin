422c147954e6b97cc5befe382b8b2bf619602b18
Muhammad Hewedy <mhewedy@gmail.com>
2019-05-20 17:22:26 +0300

    googleio18

--- src/learnkotlin/googleio18/Inline.kt
+++ src/learnkotlin/googleio18/Inline.kt
@@ -9,28 +9,28 @@ fun main() {
     println(list)
 
 /*
-    var `element$iv$iv`: Any
+    var element$iv$iv: Any
     var var9: Boolean
     while (var6.hasNext()) {
-        `element$iv$iv` = var6.next()
-        val it = (`element$iv$iv` as Number).toInt()
+        element$iv$iv = var6.next()
+        val it = (element$iv$iv as Number).toInt()
         var9 = false
         val var11 = it.toString()               // MAP <<<<<<<<<<<<<<<<<<<<<<<<<<<<
-        `destination$iv$iv`.add(var11)
+        destination$iv$iv.add(var11)
     }
 
-    `$this$filter$iv` = `destination$iv$iv` as List<*> as Iterable<*>
-    `$i$f$filter` = false
-    `destination$iv$iv` = ArrayList() as Collection<*>
-    `$i$f$filterTo` = false
-    var6 = `$this$filter$iv`.iterator()
+    $this$filter$iv = destination$iv$iv as List<*> as Iterable<*>
+    $i$f$filter = false
+    destination$iv$iv = ArrayList() as Collection<*>
+    $i$f$filterTo = false
+    var6 = $this$filter$iv.iterator()
 
     while (var6.hasNext()) {
-        `element$iv$iv` = var6.next()
-        val it = `element$iv$iv` as String
+        element$iv$iv = var6.next()
+        val it = element$iv$iv as String
         var9 = false
         if (it.length == 1) {                   // FILTER <<<<<<<<<<<<<<<<<<<<<<<<<<<<
-            `destination$iv$iv`.add(`element$iv$iv`)
+            destination$iv$iv.add(element$iv$iv)
         }
     }
 */
